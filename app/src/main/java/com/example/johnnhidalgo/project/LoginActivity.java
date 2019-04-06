dd20e82c87253700d4dee113d0fd8cc52538faad
JohnnHidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-04-06 12:04:14 -0400

    Create Admin module

--- app/src/main/java/com/example/johnnhidalgo/project/LoginActivity.java
+++ app/src/main/java/com/example/johnnhidalgo/project/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.johnnhidalgo.project;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -187,7 +188,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+            Intent h = new Intent(LoginActivity.this, MenuAdminActivity.class);
+            startActivity(h);
         }
+
     }
 
     private boolean isEmailValid(String email) {
