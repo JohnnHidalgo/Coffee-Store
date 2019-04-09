fbd3a8b3b242ef76cfa1f3067cc0f4b9d958bcc2
JohnnHidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-04-08 23:16:46 -0400

    PersonalActivity terminated

--- app/src/main/java/com/example/johnnhidalgo/project/Admin/modules/PersonalActivity.java
+++ app/src/main/java/com/example/johnnhidalgo/project/Admin/modules/PersonalActivity.java
@@ -1,16 +1,73 @@
 package com.example.johnnhidalgo.project.Admin.modules;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import com.example.johnnhidalgo.project.Database.DatabaseHelper;
 import com.example.johnnhidalgo.project.R;
+import com.example.johnnhidalgo.project.adapters.UsersRecyclerAdapter;
+import com.example.johnnhidalgo.project.modelos.User;
 
-public class PersonalActivity extends Activity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class PersonalActivity extends AppCompatActivity {
+
+    private AppCompatActivity activity = PersonalActivity.this;
+    private List<User> listUsers;
+    private RecyclerView recyclerViewUsers;
+    private UsersRecyclerAdapter usersRecyclerAdapter;
+    private DatabaseHelper db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal);
+
+
+        recyclerViewUsers = (RecyclerView) findViewById(R.id.recyclerViewUsers);
+
+        listUsers = new ArrayList<>();
+        usersRecyclerAdapter = new UsersRecyclerAdapter(listUsers);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerViewUsers.setLayoutManager(mLayoutManager);
+        recyclerViewUsers.setItemAnimator(new DefaultItemAnimator());
+        recyclerViewUsers.setHasFixedSize(true);
+        recyclerViewUsers.setAdapter(usersRecyclerAdapter);
+        db = new DatabaseHelper(activity);
+
+        getDataFromSQLite();
+
+
     }
+
+
+
+
+    private void getDataFromSQLite() {
+        // AsyncTask is used that SQLite operation not blocks the UI Thread.
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                listUsers.clear();
+                listUsers.addAll(db.getAllUser());
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                usersRecyclerAdapter.notifyDataSetChanged();
+            }
+        }.execute();
+    }
+
+
 
 }
--- app/src/main/java/com/example/johnnhidalgo/project/adapters/UsersRecyclerAdapter.java
+++ app/src/main/java/com/example/johnnhidalgo/project/adapters/UsersRecyclerAdapter.java
@@ -1,4 +1,61 @@
 package com.example.johnnhidalgo.project.adapters;
 
-public class UsersRecyclerAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.johnnhidalgo.project.R;
+import com.example.johnnhidalgo.project.modelos.User;
+
+import java.util.List;
+
+public class UsersRecyclerAdapter extends RecyclerView.Adapter<UsersRecyclerAdapter.UserViewHolder>{
+    private List<User> listUsers;
+
+    public UsersRecyclerAdapter(List<User> listUsers) {
+        this.listUsers = listUsers;
+    }
+
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.item_user_recycler, viewGroup, false);
+
+        return new UserViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder userViewHolder, int position) {
+
+        userViewHolder.textViewName.setText(listUsers.get(position).getUserName());
+        userViewHolder.textViewPassword.setText(listUsers.get(position).getUserPass());
+    }
+
+    @Override
+    public int getItemCount() {
+        Log.v(UsersRecyclerAdapter.class.getSimpleName(),""+listUsers.size());
+        return listUsers.size();
+    }
+
+
+    public class UserViewHolder extends RecyclerView.ViewHolder {
+
+        public AppCompatTextView textViewName;
+        public AppCompatTextView textViewPassword;
+
+        public UserViewHolder(View view) {
+            super(view);
+            textViewName = (AppCompatTextView) view.findViewById(R.id.textViewName);
+            textViewPassword = (AppCompatTextView) view.findViewById(R.id.textViewPassword);
+        }
+    }
+
+
+
 }
