cb049adf12d77eea389e4714dc42cada709e4d8f
JohnnHidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-04-20 16:24:20 -0400

    create pedido module in Cliente

---
+++ app/src/main/java/com/example/johnnhidalgo/project/Cliente/modules/Pedidos/PedidoList.java
@@ -0,0 +1,15 @@
+package com.example.johnnhidalgo.project.Cliente.modules.Pedidos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.johnnhidalgo.project.R;
+
+public class PedidoList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedido_list);
+    }
+}
